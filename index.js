d9a5ce93737d102c61e5a973c683249e78e8b8f3
Nofrialdi <aldi.fasive@gmail.com>
2023-01-31 09:20:00 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const menyToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menyToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});

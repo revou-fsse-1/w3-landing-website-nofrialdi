ebc318acf58e15f8d294539b48d1704dd9caf621
Nofrialdi <aldi.fasive@gmail.com>
2023-02-02 16:50:24 +0700

    added alert function when submit on click

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function submitFunction() {
+  var fullName = document.getElementById("fullName").value;
+  alert("Thank you " + fullName + " for filling out the form :)");
+}
+// navbar slide function
 const menyToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
 
